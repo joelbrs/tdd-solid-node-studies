ff8c9dabd7386a70fc4c008232e38f770af456ba
Joel Filho <joelfilho02812@gmail.com>
2023-03-15 15:27:59 -0300

    fix future dates in tests

--- src/entities/__test__/appointment.spec.ts
+++ src/entities/__test__/appointment.spec.ts
@@ -1,13 +1,11 @@
 import { expect, it } from "vitest";
 import { CreateAppointment } from "../../use-cases/create-appointment";
 import { Appointment } from "../appointment";
+import { getFutureDate } from "../../utils/get-future-date";
 
 it("create an appointment", () => {
-  const startsAt = new Date();
-  const endsAt = new Date();
-
-  startsAt.setDate(startsAt.getDate() + 1);
-  endsAt.setDate(startsAt.getDate() + 3);
+  const startsAt = getFutureDate('2023-03-15');
+  const endsAt = getFutureDate('2023-03-64');
 
   const appointment = new Appointment({
     customer: "John Doe",
@@ -20,11 +18,8 @@ it("create an appointment", () => {
 });
 
 it("cannot create an appointment with end date before start date", () => {
-  const startsAt = new Date();
-  const endsAt = new Date();
-
-  startsAt.setDate(endsAt.getDate() + 2);
-  endsAt.setDate(endsAt.getDate() + 1);
+  const startsAt = getFutureDate('2023-03-15');
+  const endsAt = getFutureDate('2023-03-14');
 
   expect(() => {
     return new Appointment({
--- src/use-cases/__test__/create-appointment.spec.ts
+++ src/use-cases/__test__/create-appointment.spec.ts
@@ -1,16 +1,14 @@
 import { describe, expect, it } from "vitest";
 import { Appointment } from "../../entities/appointment";
 import { CreateAppointment } from "../create-appointment";
+import { getFutureDate } from "../../utils/get-future-date";
 
 describe("create appointment", () => {
   it("should be able to create an appointment", () => {
     const createAppointment = new CreateAppointment();
 
-    const startsAt = new Date();
-    const endsAt = new Date();
-
-    startsAt.setDate(startsAt.getDate() + 1);
-    endsAt.setDate(startsAt.getDate() + 3);
+    const startsAt = getFutureDate('2023-03-15');
+    const endsAt = getFutureDate('2023-03-16');
 
     expect(
       createAppointment.execute({
