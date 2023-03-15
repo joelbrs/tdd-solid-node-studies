858a3d9227306c9123279918d80b8161c2a6d237
Joel Filho <joelfilho02812@gmail.com>
2023-03-15 13:01:36 -0300

    add get future date utils function

---
+++ src/utils/get-future-date.ts
@@ -0,0 +1,9 @@
+import { setYear, parseISO } from "date-fns";
+
+/**
+ * receives: "2023-03-15" and returns "2024-08-15"
+ */
+
+export function getFutureDate(date: string) {
+  return setYear(parseISO(date), new Date().getFullYear() + 1);
+}
